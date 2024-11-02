caf4554e5bf24f911b5cd6bcce8fc786fbf7773b
TomasX59 <tcercarodrigues@gmail.com>
2024-11-02 15:18:53 +0000

    .

---
+++ Aula06/aula6_2.asm
@@ -0,0 +1,36 @@
+# Mapa de registros:
+# p		:	$t1
+# pultimo	:	$t2
+
+
+
+	.eqv	SIZE,3
+	.eqv	print_string,4
+	.eqv	print_char,11
+
+	.data
+
+array:	.word	str1,str2,str3
+str1:	.asciiz "Array"
+str2:	.asciiz "de"
+str3:	.asciiz "ponteiros"
+	.text
+	.globl main
+
+main:
+
+	la 	$t1,array 		# $t1 = p = &array[0] = array 
+	li	$t0,SIZE		#
+	sll 	$t0,$t0,2 		#
+ 	addu 	$t2,$t1,$t0		# $t2 = pultimo = array + SIZE
+
+for:	bge 	$t1,$t2,endfor
+
+	ori	$v0,0,print_string
+	lw	$a0,0($t1)
+
+
+
+	addi	$t1,$t1,1
+endfor:
+	jr	$ra
